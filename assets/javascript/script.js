405f5c941de07185a007d063bf068da864e1097a
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-22 20:56:26 -0300

    commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,23 +3,27 @@ let bigCupImage = document.querySelector('.starbucks')
 let greenCup = document.querySelector('.thumb1')
 let lightPinkCup = document.querySelector('.thumb2') // mudar .src e background
 let pinkCup = document.querySelector('.thumb3')
+let spanTitle = document.querySelector('.span')
 
 greenCup.addEventListener('click', () => {
   bigCupImage.src = '../../images/Starbucks-Coffee-PNG-Transparent.png'
   bgBigCup.style.background = '#017143'
   bgBigCup.style.transition = '.7s'
+  spanTitle.style.color = '#017143'
 })
 
 lightPinkCup.addEventListener('click', () => {
   bigCupImage.src = '../../images/big-two.png'
   bgBigCup.style.background = '#F2CFD7'
   bgBigCup.style.transition = '.7s'
+  spanTitle.style.color = '#F2CFD7'
 })
 
 pinkCup.addEventListener('click', () => {
   bigCupImage.src = '../../images/big-three.png'
   bgBigCup.style.background = '#E060B9'
   bgBigCup.style.transition = '.7s'
+  spanTitle.style.color = '#E060B9'
 })
 /*function trocaImagem(cor) {
   let circ = document.getElementsByClassName('circulo')
